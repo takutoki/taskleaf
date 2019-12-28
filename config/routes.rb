3ef37097baab8cc8697911073f21e15f70d1d085
takutoki <34600200+takutoki@users.noreply.github.com>
2019-12-28 22:26:04 +0900

    create user with admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+  
   root to: 'tasks#index'
   resources :tasks
-  # get 'tasks/index'
-  # get 'tasks/show'
-  # get 'tasks/new'
-  # get 'tasks/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
