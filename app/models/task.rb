40a9dd0a15c9d47b672ed6006a005033e35392ea
takutoki <34600200+takutoki@users.noreply.github.com>
2020-01-05 17:05:22 +0900

    add search task

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,5 +9,15 @@ class Task < ApplicationRecord
   def validate_name_not_include_comma
     errors.add(:name, 'カンマは含めることができません') if name&.include?(',')
   end
+
+  # ransackで検索できるカラムを絞る
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  # 検索条件に含める関連
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
 
