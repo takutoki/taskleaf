3556e76e3d866d5e83323189edfc2249be74b6a5
takutoki <34600200+takutoki@users.noreply.github.com>
2020-01-05 16:18:03 +0900

    add comfirm page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,8 +15,18 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = Task.new(task_params.merge(user_id: current_user.id))
+
+    if params[:back].present?
+      render :new
+      return
+    end
 
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
   
   root to: 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    post '/confirm', action: :confirm_new, on: :new
+  end
 end
